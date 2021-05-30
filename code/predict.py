fb2c9a7d8c49debe49174dd7b3227a17f9c40ea5
Aizaz Arif Ansari <aizazarifansari@gmail.com>
2021-05-30 23:37:15 +0400

    adding predict.py funcs

--- code/predict.py
+++ code/predict.py
@@ -1,85 +1,72 @@
 import os 
 import subprocess
+import configparser
 
 
-def get_bookname():
-	books = os.listdir("data")
-	for i,book in enumerate(books):
-		print(i+1," - ",book)
-
-	booknumber = input("Select the book: ")
-	bookname = books[int(booknumber)-1]
-	print(bookname, " selected")
-	bookname = "data/" + bookname
-	return bookname
-
-def get_model():
-	
-	models = os.listdir("models")
-	models.sort()
-	for i,model in enumerate(models):
-		print(i+1," - ",model)
-
-	modelnumber = input("Select the model: ")
-	modelname = models[int(modelnumber)-1]
-	print(modelname, " selected")
-	modelname = "models/" + modelname
-
-
-def get_mapping():
-	print("1 - Segmenter")
-	print("2 - Visual")
-	print("3 - Empirical")
-	mappingtype = input("Enter the type of mapping: ")
-	if mappingtype == "1":
-		mappingname = "mappings/segmenter.map"
-	elif mappingtype == "2":
-		mappings = os.listdir("mappings/visual")
-		mappings.sort()
-		for i,mapping in enumerate(mappings):
-			print(i+1," - ",mapping)
-
-		mappingnumber = input("Select the visual mapping: ")
-		mappingname = mappings[int(mappingnumber)-1]
-		print(mappingname, " selected")
-		mappingname = "mappings/visual/" + mappingname
-	else:
-		mappings = os.listdir("mappings/empirical")
-		mappings.sort()
-		for i,mapping in enumerate(mappings):
-			print(i+1," - ",mapping)
-
-		mappingnumber = input("Select the empircal mapping: ")
-		mappingname = mappings[int(mappingnumber)-1]
-		print(mappingname, " selected")
-		mappingname = "mappings/empircal/" + mappingname
-
-def get_range():
-	pages = os.listdir(bookname+"/ocr/")
-	print("Number of pages available to post-process: ",len(pages))
-	start_page = input("Enter start page for prediction: ")
-	end_page = input("Enter end page for prediction: ")
-
-def encode();
+def encode(line):
+	new_l = []
+    line = araby.strip_shadda(araby.strip_harakat(line)).replace("آ", "ا").replace("إ", "ا").replace("أ", "ا")
+    new_line = ""
+    for letter in line:
+        if (ord(letter) < begin or ord(letter) > end) and (letter!=' '):
+            continue
+        new_line = new_line + letter
+    line = new_line
+    
+    for w in line.split():
+        i = 0
+        for c in w:
+            if len(w)==1:
+                new_l.append(c)
+            elif i == len(w) - 1:
+                new_l.append("#" + c)
+            elif i == 0:
+                new_l.append(c + "#")
+            else:
+                new_l.append("#" + c + "#")
+            i += 1
+    return "<s> " + " ".join(new_l) + " </s>"
 
 def write_encoded():
+	try:
+		os.mkdir("data/" + parameters[book_name]+ "/" + parameters[book_name] + "_ocr_encoded/")
+	except:
+		pass
+
+	for i in range(parameters[start_page],parameters[end_page]+1):
+		original_file = open("data/" + parameters[book_name]+ "/" + parameters[book_name] +"_ocr/" + "ocr_" + str(i) + ".txt")
+		encoded_text = encode(original_file.read())
+		encoded_file = open("data/" + parameters[book_name]+ "/" + parameters[book_name] + "_ocr_encoded/" + "ocr_encoded_" + str(i) + ".txt")
+		encoded_file.write(encoded_text)
+		original.close()
+		encoded_file.close()
 
 
 def predict():
-	spec_prefix = bookname + "_" + modelname + "_" + mappingname
-	os.mkdir(bookname+"/post_edited/")
-	os.mkdir(bookname+"/post_edited/"+spec_prefix)
+	spec_prefix = parameters[book_name] + "_" + parameters[model_name] + "_" + parameters[mapping_name]
+	os.mkdir("data/" + parameters[book_name] +"/" + parameters[book_name] + "_post_edited/")
+	prediction_path = "data/" + parameters[book_name] + "/" + parameters[book_name] + "_post_edited/"+ parameters[book_name] + "_" spec_prefix + "/"
+	os.mkdir(prediction_path)
+
 
 	for i in range(start_page,end_page+1):
-		print("wow")
+		raw_ocr_arg = "data/" + parameters[book_name]+ "/" + parameters[book_name] + "_ocr_encoded/" + "ocr_encoded_" + str(i) + ".txt"
+		predicted_arg = prediction_path + "predicted_" + str(i) + ".txt"
+        model_arg = "-lm /models/" + parameters[model_name] + " "
+        map_arg = "-map mappings" + parameters[mapping_name] " "
+        order_arg = "-order 8 "
+        text_files = "-text " + raw_ocr_arg + ">" + predicted_arg
+
+        command = "/Users/aizazansari/Downloads/srilm-1.7.3/bin/macosx/disambig " + model_name + "-keep-unk " + order_name + map_name + text_files
+        p = subprocess.getstatusoutput(command)
 
 
-bookname = ""
+
 os.chdir("..")
-bookname = get_bookname()
-print()
-get_model()
-print()
-get_mapping()
-print()
-get_range()
+
+config = configparser.RawConfigParser()
+config.read('config_files/default.cfg')    
+parameters = dict(config.items('predict'))
+
+
+
