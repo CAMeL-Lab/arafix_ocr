5f893890d9a5aa124276f86877d7ce10b3934cff
Aizaz Arif Ansari <aizazarifansari@gmail.com>
2021-07-07 14:48:57 +0500

    prediction mapping error fixed

--- code/predict_dalma.py
+++ code/predict_dalma.py
@@ -51,10 +51,23 @@ def check_parameters():
 		print("Model does not exist!")
 		incorrect = True
 
-	mappings = os.listdir("mappings")
-	if parameters["map_name"] not in mappings:
-		print("Mapping does not exist!")
-		incorrect = True
+	if parameters["map_name"].startswith("empirical"):
+		mappings = os.listdir("mappings/empirical")
+		if parameters["map_name"].strip("empirical/") not in mappings:
+			print("Mapping does not exist!")
+			incorrect = True
+
+	else if parameters["map_name"].startswith("visual"):
+		mappings = os.listdir("mappings")
+		if parameters["map_name"].strip("visual/") not in mappings:
+			print("Mapping does not exist!")
+			incorrect = True
+
+	else:
+		mappings = os.listdir("mappings")
+		if parameters["map_name"] not in mappings:
+			print("Mapping does not exist!")
+			incorrect = True
 
 	if incorrect:
 		exit(0)
