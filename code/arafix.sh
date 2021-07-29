4bd52e17fc8a17a05b08f2c64b8179415bdfa7e1
Anas Jawed <anasjawed@Anass-MacBook-Pro.local>
2021-07-29 14:17:05 +0500

    riadh stuff

--- code/arafix.sh
+++ code/arafix.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-config_name="default.txt"
-book_name=$1
-start_page="None"
-end_page="None"
+config_name="flexible.txt"
+book_name="columbia_aco000270"
+start_page="9"
+end_page="29"
 
 echo ""
 
 echo "~~~RUNNING ARAFIX FOR BOOK: ${book_name}~~~"
 
-python image_to_text.py \
--config "${config_name}" \
--bookname "${book_name}" \
--startpage "${start_page}" \
--endpage "${end_page}" 
+# python image_to_text.py \
+# -config "${config_name}" \
+# -bookname "${book_name}" \
+# -startpage "${start_page}" \
+# -endpage "${end_page}" 
 
 
 # python predict.py \
