43c99552420e98f3b3ac1000c71c2484ac5c1de4
Anas Jawed <anasjawed@Anass-MacBook-Pro.local>
2021-09-20 13:31:10 +0500

    license update

--- code/evaluate.py
+++ code/evaluate.py
@@ -1,38 +1,26 @@
-#MIT License
-
 # Copyright 2021 New York University Abu Dhabi
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+#     http://www.apache.org/licenses/LICENSE-2.0
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import subprocess
 import pandas as pd
 import csv
 import camel_tools.utils.charsets
-import pyarabic.araby as araby
-import pyarabic.number as number
+from camel_tools.utils.dediac import dediac_ar, dediac_bw
 import difflib
 import argparse
 import configparser
-import pyarabic.araby as araby
-import pyarabic.number as number
 import shutil
 import re
 
@@ -156,7 +144,8 @@ def strip_text(raw_text):
 	no_punc_text = "".join([c for c in raw_text.replace("\n", " ") if (c not in arabic_punctuation and 1536 <= ord(c) <= 1791) or c == " " ])
 	no_punc_text = re.sub(" +", " ", no_punc_text)
 
-	no_punc_text = araby.strip_shadda(araby.strip_harakat(no_punc_text)).replace("آ", "ا").replace("إ", "ا").replace("أ", "ا")
+	# no_punc_text = araby.strip_shadda(araby.strip_harakat(no_punc_text)).replace("آ", "ا").replace("إ", "ا").replace("أ", "ا")
+	no_punc_text = dediac_ar(no_punc_text).replace("آ", "ا").replace("إ", "ا").replace("أ", "ا")
 
 	return no_punc_text
 
---
+++ license.md
@@ -0,0 +1,6 @@
+## License
+
+Copyright 2021 New York University Abu Dhabi.
+
+arafix_ocr is available under the Apache License, Version 2.0.
+See the [LICENSE](/LICENSE) file for more info.
